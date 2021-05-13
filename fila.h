fe010c789f26924dc1bf2b3f05f2cf24db01b07f
otavi <otavio.366@hotmail.com>
2021-05-13 15:43:28 -0300

    tentando arrumar erro

---
+++ fila.h
@@ -0,0 +1,15 @@
+void enfileirar(int valor);
+
+int desenfileirar();
+
+int vazia();
+
+int cheia();
+
+void exibir();
+
+<<<<<<< HEAD
+void limpar();
+=======
+void limpar();
+>>>>>>> 6653e94b7e0d68ba2bd8562934f75e60c123800c
