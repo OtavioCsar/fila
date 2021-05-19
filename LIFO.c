bdf04791b22df164f9c008e209b342edde2bd81a
otavi <otavio.366@hotmail.com>
2021-05-19 20:28:56 -0300

    codigo certo?

--- LIFO.c
+++ LIFO.c
@@ -4,7 +4,7 @@
 
 #define TAM_FILA 3
 
-int fila[TAM_FILA], ultimaPosicao = 0;
+int fila[TAM_FILA], ultimaPosicao = TAM_FILA - 1;
 
 int main(void)
 {
@@ -17,85 +17,69 @@ int main(void)
 
     return 0;
 }
-
 void enfileirar(int valor)
 {
-
-    if (cheia()) {
-
-        printf("Fila cheia!");
-
-    } else {
-
+    if (cheia() == 0) {
         fila[ultimaPosicao] = valor;
-        ultimaPosicao++;
+        ultimaPosicao--;
+    } else {
+        printf("Nao foi possivel enfileirar item. Lista cheia!");
     }
 }
 
 int desenfileirar()
 {
-
-    int itemRetirado = 0;
-
-    if (vazia()) {
-
-        printf("Fila vazia!");
-
-    } else {
-
-        itemRetirado = fila[0];
-
-        //Fazer a fila andar
-        for (int i = 0; i > ultimaPosicao - 1; i++) {
-            fila[i] = fila[i + 1];
+    int valor;
+    if (vazia() == 0) {
+        for (int i = 0; i < TAM_FILA; i++) {
+            fila[i] = 0;
+            if (i + 1 < TAM_FILA && fila[i + 1]) {
+                fila[i] = fila[i + 1];
+            }
         }
-
-        ultimaPosicao--;
+        ultimaPosicao++;
+    } else {
+        printf("Não foi possivel desenfileirar item. Lista vazia!");
     }
-
-    return itemRetirado;
 }
 
 int vazia()
 {
-
-    if (ultimaPosicao == 0) {
-        return 1;
-    } else {
-        return 0;
+    int resultado = 1;
+    //Se resultado=1 significa que todos estão vazios e se for = 0 ele tem algum espaço cheio.
+    for (int i = 0; i < TAM_FILA; i++) {
+        //Checa cada item para ver se não estão vazios
+        if (fila[i] != 0) {
+            resultado = 0;
+        }
     }
+    return resultado;
 }
 
 int cheia()
 {
-
-    if (ultimaPosicao == TAM_FILA) {
-        return 1;
-    } else {
-        return 0;
+    int resultado = 1;
+    //Se resultado=1 significa que está cheio e se for = 0 ele tem algum espaço vazio.
+    for (int i = 0; i < TAM_FILA; i++) {
+        //Se algum item estiver vazio resultado recebe 0
+        if (fila[i] == 0) {
+            resultado = 0;
+        }
     }
+    //Aqui retorna o valor
+    return resultado;
 }
 
 void exibir()
 {
 
-    if (vazia()) {
-
-        printf("Fila vazia!");
-
-    } else {
-
-        for (int i = 0; i < ultimaPosicao; i++) {
-            printf("%i\n", fila[i]);
-        }
+    for (int i = 0; i < TAM_FILA; i++) {
+        printf("%i\n", fila[i]);
     }
 }
-
-void esvaziar()
+void limpar()
 {
-
-    // Enquanto a fila não estiver vazia
-    while (!vazia()) {
+    for (int i = 0; i < TAM_FILA; i++) {
         desenfileirar();
     }
 }
